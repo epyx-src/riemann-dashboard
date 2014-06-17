f0e983b86e3292fe4f5cd6a754505664f57a3bf1
David Saradini <david.saradini@me.com>
2014-06-17 18:04:53 +0200

    Corrected config
    Signed-off-by: David Saradini <david.saradini@me.com>

--- www/js/config.js
+++ www/js/config.js
@@ -1,4 +1,4 @@
 window.SSE_URL = 'https://sentry.exoscale.ch:8843/index/';
-window.GRAPHITE_URL = 'https://graphite.exodoc.ch/render/'
+window.GRAPHITE_URL = 'http://185.19.28.165:9126/render/'
 
 window.REFRESH_RATE = 5000;
