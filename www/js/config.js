a2be8f67a40c6dd26aa684cb7bad8fbc8ce30d62
David Saradini <david@saradini.ch>
2015-02-05 10:00:51 +0100

    Do not use SSL for SSE
    Signed-off-by: David Saradini <david@saradini.ch>

--- www/js/config.js
+++ www/js/config.js
@@ -1,4 +1,4 @@
-window.SSE_URL = 'https://sentry.epyx.ch:8843/index/';
+window.SSE_URL = 'http://185.19.28.17:5558/index/';
 window.GRAPHITE_URL = 'http://185.19.28.165:9126/render/'
 
 window.REFRESH_RATE = 5000;
