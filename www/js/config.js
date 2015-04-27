a6b55b88ebc8798e30aa6d9e7f862e2375d06f0d
Yann Savary <yann.savary@epyx.ch>
2015-04-27 18:03:42 +0200

    Fixed security issue where anyone can access to http://graphite.exodoc.ch

--- www/js/config.js
+++ www/js/config.js
@@ -1,4 +1,4 @@
 window.SSE_URL = 'https://sentry.epyx.ch:8843/index/';
-window.GRAPHITE_URL = 'http://185.19.28.165:9126/render/';
+window.GRAPHITE_URL = 'https://graphite.exodoc.ch/render/';
 
 window.REFRESH_RATE = 5000;
